9be272257de5798a9b24725192609bc032390d36
Aman Momin <mominaman718@gmail.com>
2025-06-17 23:12:57 +0530

    edit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Cineme:\n  1 2 3 4 5 6 7 8");
+        System.out.println("Cineme:\n  1 2 4 5 6 7 8");
 
         for(int i=1; i<8; i++){
             System.out.println(i+" S S S S S S S S");
