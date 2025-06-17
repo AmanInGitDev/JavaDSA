f7977c6a40d8983ac7cf18a375d3f611af4fef9f
aman <mominaman718@gmail.com>
2025-06-18 00:07:21 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Striver_Basics/V_BasicString/rotateStringOptimal.java
+++ src/Striver_Basics/V_BasicString/rotateStringOptimal.java
@@ -1,21 +1,21 @@
-public class rotateStringBruite {
+package Striver_Basics.V_BasicString;
+
+public class rotateStringOptimal {
     public boolean rotateString(String s, String goal) {
 
         if (s.length() != goal.length()) {
             return false;
         }
 
-        for (int i = 0; i < s.length(); i++) {
-            String rotated = s.substring(i) + s.substring(0, i);
-            if (rotated.equals(goal)) {
-                return true;
-            }
+        String ans = s + s;
+        if(ans.contains(goal)){
+            return true;
         }
         return false;
     }
 
     public static void main(String[] args) {
-        rotateStringBruite sol = new rotateStringBruite();
+        rotateStringOptimal sol = new rotateStringOptimal();
         System.out.println(sol.rotateString("abcde", "cdeab"));
         System.out.println(sol.rotateString("abcde", "abced"));
     }
