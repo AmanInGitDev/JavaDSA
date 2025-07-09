a5198a1b15ec6fdd0a173403640fbee9324a6668
aman <iamaman2901@gmail.com>
2025-07-09 12:19:43 +0530

    Merge remote-tracking branch 'origin/main'

--- src/Striver_Basics/IV_BasicHash/MostOccuBrute.java
+++ src/Striver_Basics/IV_BasicHash/MostOccuBrute.java
@@ -1,43 +1,94 @@
+/*Highest Occurring Element in an Array
+Given an array of n integers, find the most frequent element in it i.e., the element that occurs the maximum number of times.
+If there are multiple elements that appear a maximum number of times, find the smallest of them.
+
+Please note that this section might seem a bit difficult without prior knowledge on what hashing is, we will soon try to add basics concepts for your ease!
+If you know the concepts already please go ahead to give a shot to the problem. Cheers!
+
+Examples:  Input: arr = [1, 2, 2, 3, 3, 3]
+Output: 3
+
+Explanation: The number 3 appears the most (3 times). It is the most frequent element.*/
+
 class MostOccuBrute {
-
-    public int solution1(int[] nums){
-        int n = nums.length;         // Its length of nums[] so that we can use it for looping
-        int maxEle = 0;              // It's used to store and update the maximum element found and initially is 0;
-        int maxfreq = 0;             // It's used to store the maximum frequency of an element, and It's also initially 0;
-
-        boolean[] visited = new boolean[n];   // It's to check whether the element is visited and if visited it will marked true later
-
-        for(int i = 0; i < n; i++){       // All the operation will be performed for index 0 to n (size of an array)
-
-            if (visited[i]) continue;       // Here we check the condition that the i element is visited then its continued
-            int freq = 0;                    // Simply check the frequency of i only (not the max)
-
-            for (int j = i; j < n; j++){       // Inner loop to traverse and find the i element which the n elements;
-
-                if (nums[i] == nums[j]) {      // It means the number is found
-                    freq++;                   // freq is updated and increased by one
-                    visited[j] = true;         // Means the element is visited and its true now
+    
+    public int mostFrequentElement(int[] nums) {
+        int n = nums.length;
+        int maxCount = 0;
+        int maxEle = 0;
+        
+        boolean[] vis = new boolean[n];
+        
+        for(int i = 0; i < n; i++){
+            if (vis[i]) continue;
+            
+            int count = 0;
+            
+            for (int j = i; j < n; j++){
+                if (nums[i] == nums[j]){
+                    count++;
+                    vis[j] = true;
                 }
-
-                if (freq > maxfreq){           // It's to check whether the the freq of element is greater than maxfreq or its may less if great then following exuuted
-                    maxfreq = freq;            // If its become true then update the maxFreq.
-                    maxEle = nums[i];          // Update max elementh to its i in nums.
-                }
-
-                else if (freq == maxfreq){                   // Here it may be equal then
-                    maxEle = Math.min(maxEle, nums[i]);      // Compare the maxEle, As per question whichever is smaller we have tp retuen this
-                                                             //so use math.min function and provide (two values) one is the maxEle and another is one we got in nums as i.
-                }
+            }
+            
+            if (count > maxCount) {
+                maxCount = count;
+                maxEle = nums[i];
+            }
+            else if (count == maxCount){
+                maxEle = Math.min(maxEle, nums[i]);
             }
         }
         return maxEle;
     }
-
+    
     public static void main(String [] args){
-        int[] nums = { 7,3,2,3,2,3,5};
-
+        int[] nums = {4, 4, 5, 5, 6};
+        
         MostOccuBrute Sol1 = new MostOccuBrute();
-        int answer1 = Sol1.solution1(nums);
+        int answer1 = Sol1.mostFrequentElement(nums);
         System.out.println(answer1);
     }
 }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
